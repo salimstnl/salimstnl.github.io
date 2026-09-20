0dca12a04b34876575d57552f93a2d976245b768
Stanley Salim <stanley.salim@hotmail.com>
2026-09-20 23:35:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const observerOptions = {
         root: null,
         rootMargin: "0px",
-        threshold: 0.15 // Triggers when 15% of the element is visible
+        threshold: 0.15
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("show");
-                observer.unobserve(entry.target); // Runs once per element
+                observer.unobserve(entry.target);
             }
         });
     }, observerOptions);
 
-    // Target all sections and project cards for the fade-in effect
-    const hiddenElements = document.querySelectorAll('.section, .project-card, .video-container');
+    // Target all sections, project cards, video containers, and press cards for the fade-in effect
+    const hiddenElements = document.querySelectorAll('.section, .project-card, .video-container, .press-card');
+    
     hiddenElements.forEach((el) => {
         el.classList.add('hidden');
         observer.observe(el);
